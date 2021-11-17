7cb8a9bd652edffe30848d6b1199df8153d6b363
Vaibhav Shinde <vaibhavshinde1642@gmail.com>
2021-11-17 09:37:37 +0530

    initial commit

---
+++ src/main/java/com/ashokit/S35AMicroservicesCircuitBreakerDesignPatternApplication.java
@@ -0,0 +1,13 @@
+package com.ashokit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S35AMicroservicesCircuitBreakerDesignPatternApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S35AMicroservicesCircuitBreakerDesignPatternApplication.class, args);
+	}
+
+}
